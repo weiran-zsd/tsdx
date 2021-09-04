e1936ec68136f02165541b0ed019a871a7923b7f
Sidharth Rathi <58144379+sidwebworks@users.noreply.github.com>
2021-09-04 15:01:28 +0530

    Update src/messages.ts
    Co-authored-by: 薛定谔的猫 <weiran.zsd@outlook.com>

--- src/messages.ts
+++ src/messages.ts
@@ -20,7 +20,7 @@ export const missingProjectName = function () {
 Please specify the project directory:
   ${chalk.cyan(program.name)} ${chalk.green('<project-directory>')}
 For example:
-  ${chalk.cyan(program.name)} ${chalk.green('my-dts-cli-lib')}
+  ${chalk.cyan(program.name)} ${chalk.green('my-dts-lib')}
 Run ${chalk.cyan(`${program.name} --help`)} to see all options.
 `;
 };
