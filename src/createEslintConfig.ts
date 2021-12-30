de69de041ef022104115fcbec0f7815b2d8728cc
唯然 <weiran.zsd@outlook.com>
2021-12-30 15:03:32 +0800

    chore: add tests for issue 118

--- src/createEslintConfig.ts
+++ src/createEslintConfig.ts
@@ -34,7 +34,7 @@ export async function createEslintConfig({
   }
 
   const file = path.join(rootDir, '.eslintrc.js');
-  // it the config is an abs path(e.g. "/Users/user/my-project/.eslintrc.js"),
+  // if the path is an abs path(e.g. "/Users/user/my-project/.eslintrc.js"),
   // need to convert a rel path to app root.
   config.extends = config.extends.map((it) =>
     /^\//u.test(it) ? path.relative(rootDir, it) : it
