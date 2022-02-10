60b72300a9760591248eae41a0c8ac3ab95044ba
Richard Harrah <topplethenunnery@gmail.com>
2022-02-10 17:53:32 -0500

    test: use native typescript syntax for dts.config.ts

--- test/integration/fixtures/build-withConfigTs/dts.config.ts
+++ test/integration/fixtures/build-withConfigTs/dts.config.ts
@@ -1,19 +1,25 @@
 import { DtsOptions, RollupOptions } from '../src';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import postcss from 'rollup-plugin-postcss';
 
-const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+// This is necessary due to how typechecking works with the @types/cssnano
+// package. If you remove the `if` check below and attempt to add the cssnano
+// processor directly, you run into issues with type stack depth.
+const getPlugins = () => {
+  const plugins: any[] = [autoprefixer()];
+  const cssnanoProcessor = cssnano({ preset: 'default' });
+  if ('version' in cssnanoProcessor) {
+    plugins.push(cssnanoProcessor);
+  }
+  return plugins;
+};
 
 export default {
   rollup(config: RollupOptions, options: DtsOptions) {
     config?.plugins?.push(
       postcss({
-        plugins: [
-          autoprefixer(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
+        plugins: getPlugins(),
         inject: false,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
