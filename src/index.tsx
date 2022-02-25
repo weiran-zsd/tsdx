64573148cf1b5c022b7531498d0cb8995151ebf7
唯然 <weiran.zsd@outlook.com>
2022-02-25 10:47:10 +0800

    repro issue 137

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { foo } from "@Components/foo";
+
+console.log(foo);
+
+// Delete me
+export const Thing = () => {
+  return <div>Welcome to your first test package.</div>;
+};
