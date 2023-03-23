b8cb41714cc43214fb53c68abd9aac7dd4acbbd2
唯然 <weiran.zsd@outlook.com>
2023-03-23 17:52:15 +0800

    feat!: upgrade storybook v7
    it also update the builder to vite(rollup-based).
    refs: https://github.com/weiran-zsd/dts-cli/issues/121

--- src/getInstallArgs.ts
+++ src/getInstallArgs.ts
@@ -4,6 +4,9 @@ export default function getInstallArgs(
   cmd: InstallCommand,
   packages: string[]
 ) {
+  // replace 'package#version' with 'package@version'
+  packages = packages.map(pkg => pkg.replace(/#/, '@'));
+
   switch (cmd) {
     case 'npm':
       return ['install', ...packages, '--save-dev'];
