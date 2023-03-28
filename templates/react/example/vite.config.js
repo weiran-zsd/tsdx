4319e44f9e85e450e200edbfcf90d822b15a711d
唯然 <weiran.zsd@outlook.com>
2023-03-28 15:44:14 +0800

    fix: @tsconfig/cra => vite-react

--- templates/react/example/vite.config.js
+++ templates/react/example/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
-import ReactPlugin from 'vite-preset-react';
+import ReactPlugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    ReactPlugin({
-      injectReact: false,
-    }),
-  ],
+  resovle: {
+    alias: {
+      react: '../node_modules/react',
+      'react-dom': '../node_modules/react-dom/profiling',
+      'scheduler/tracing': '../node_modules/scheduler/tracing-profiling',
+    },
+  },
+  plugins: [ReactPlugin()],
 });
